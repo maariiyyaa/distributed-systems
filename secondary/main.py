bc79a3fffb939a40edbb4a81a3b24147a13717db
maariiyyaa <bondar.m.2000@gmail.com>
2022-06-05 14:33:57 +0200

    secondary server

---
+++ secondary/main.py
@@ -0,0 +1,35 @@
+import requests
+from flask import Flask
+import os
+from flask import request
+
+
+LOCAL_STORAGE = {}
+app = Flask('secondary')
+
+
+@app.route('/secondary', methods=['GET', 'POST'])
+def secondary_app():
+    if request.method == 'POST':
+        try:
+            if request.is_json:
+                LOCAL_STORAGE[list(request.json.keys())[0]] = list(request.json.values())[0]
+        except Exception as e:
+            raise e
+        else:
+            return 'stored'
+
+    elif request.method == 'GET':
+        try:
+            messages = list(LOCAL_STORAGE.values())
+        except Exception as e:
+            raise e
+        else:
+            return '\t'.join(messages)
+
+
+if __name__ == '__main__':
+    if not os.environ.get("master"):
+        os.environ["master"] = "localhost"
+    requests.request('LINK', f'http://{os.environ.get("master")}:5000/register')
+    app.run(host='0.0.0.0', port=8000)
