9e0b981bec294a86b8766278ff39e9556395af4a
Mariia <bondar.m.2000@gmail.com>
2022-06-20 17:00:35 +0300

    pess lock docker run

---
+++ pes_locks/main.py
@@ -0,0 +1,24 @@
+from hazelcast.client import HazelcastClient
+import time
+
+client = HazelcastClient()
+distributed_map = client.get_map("map_pes").blocking()
+
+key = "1"
+distributed_map.put_if_absent(key, 0)
+
+print("Starting")
+
+for i in range(10):
+    distributed_map.lock(key)
+    try:
+        value = distributed_map.get(key)
+        value += 1
+        distributed_map.put(key, value)
+    finally:
+        distributed_map.unlock(key)
+
+time.sleep(2)
+print("Finished! Result =", distributed_map.get(key))
+
+client.shutdown()
